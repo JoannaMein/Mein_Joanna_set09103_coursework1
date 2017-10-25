8ba06d375bbbf41e82c3c43757289ad89f13cef1
Joanna Mein <jo@mein.co.uk>
2017-10-25 20:28:05 +0100

    Web-App File

--- sourcecode/index.py
+++ sourcecode/index.py
@@ -4,43 +4,43 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html'), 200
   
 @app.route('/about/')
 def about():
-  return render_template('about.html')
+  return render_template('about.html'), 200
 
 @app.route('/recipes/')
 def recipes():
-  return render_template('recipes.html')
+  return render_template('recipes.html'), 200
 
 @app.route('/recipes/halloween/')
 def halloween():
-  return render_template('halloween.html')
+  return render_template('halloween.html'), 200
   
 @app.route('/recipes/bonfire/')
 def bonfire():
-  return render_template('bonfire.html')
+  return render_template('bonfire.html'), 200
   
 @app.route('/recipes/christmas/')
 def christmas():
-  return render_template('christmas.html')
+  return render_template('christmas.html'), 200
 
 @app.route('/recipes/10_mins/')
 def ten_mins():
-  return render_template('10_mins.html')
+  return render_template('10_mins.html'), 200
   
 @app.route('/recipes/10_20mins/')
 def ten_twenty_mins():
-  return render_template('10_20mins.html')
+  return render_template('10_20mins.html'), 200
   
 @app.route('/recipes/20_30mins/')
 def twenty_thirthy_mins():
-  return render_template('20_30mins.html')
+  return render_template('20_30mins.html'), 200
   
 @app.route('/recipes/30_40mins/')
 def thirty_fourty_mins():
-  return render_template('30_40mins.html')
+  return render_template('30_40mins.html'), 200
   
 @app.route('/recipes/pumpkin_pie/')
 @app.route('/recipes/halloween/pumpkin_pie/')
@@ -57,7 +57,7 @@ def pumpkin_pie():
        print item
        p = item
 
-  return render_template('pumpkin_pie.html', pumpkin=p)
+  return render_template('pumpkin_pie.html', pumpkin=p), 200
   
 @app.route('/recipes/cobweb_cake/')
 @app.route('/recipes/halloween/cobweb_cake/')
@@ -74,7 +74,7 @@ def cobweb_cake():
        print item
        p = item
 
-  return render_template('cobweb_cake.html', cobweb=p)
+  return render_template('cobweb_cake.html', cobweb=p), 200
 	
 @app.route('/recipes/rocky_road/')
 @app.route('/recipes/halloween/rocky_road/')
@@ -92,7 +92,7 @@ def rocky():
        print item
        p = item
 
-  return render_template('rocky_road.html', rocky=p)
+  return render_template('rocky_road.html', rocky=p), 200
   
 @app.route('/recipes/peanut_squares/')
 @app.route('/recipes/bonfire/peanut_squares/')
@@ -109,7 +109,7 @@ def peanut_squares():
        print item
        p = item
 
-  return render_template('peanut_squares.html', peanut=p)
+  return render_template('peanut_squares.html', peanut=p), 200
   
 @app.route('/recipes/toffee_muffins/')
 @app.route('/recipes/bonfire/toffee_muffins/')
@@ -126,7 +126,7 @@ def toffee_muffins():
        print item
        p = item
 
-  return render_template('toffee_muffins.html', toffee=p)
+  return render_template('toffee_muffins.html', toffee=p), 200
   
 @app.route('/recipes/christmas_wreaths/')
 @app.route('/recipes/christmas/christmas_wreaths/')
@@ -143,7 +143,7 @@ def christmas_wreaths():
        print item
        p = item
 
-  return render_template('christmas_wreaths.html', wreaths=p)
+  return render_template('christmas_wreaths.html', wreaths=p), 200
 
 @app.route('/recipes/gingerbread_baubles/')
 @app.route('/recipes/christmas/gingerbread_baubles/')
@@ -160,7 +160,7 @@ def gingerbread_baubles():
        print item
        p = item
 
-  return render_template('gingerbread_baubles.html', baubles=p)
+  return render_template('gingerbread_baubles.html', baubles=p), 200
   
 @app.route('/recipes/mince_pies/')
 @app.route('/recipes/christmas/mince_pies/')
@@ -177,7 +177,7 @@ def mince_pies():
        print item
        p = item
 
-  return render_template('mince_pies.html', mince=p)
+  return render_template('mince_pies.html', mince=p), 200
   
 @app.errorhandler(404)
 def page_not_found(error):
